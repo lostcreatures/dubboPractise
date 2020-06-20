bf0b68b35d45536a907139d80e8baf866e3c62b4
admin <admin@SC-201910161516>
2020-06-20 15:47:35 +0800

    测试从origin/dev上checkout出dev后提交和push

--- dubbo-provider/src/com/dubbo/provider/com/dubbo/provider/impl/HelloWorldProvider.java
+++ dubbo-provider/src/com/dubbo/provider/com/dubbo/provider/impl/HelloWorldProvider.java
@@ -3,11 +3,20 @@ package com.dubbo.provider.com.dubbo.provider.impl;
 import com.dubbo.provider.Service;
 
 public class HelloWorldProvider implements Service {
+    /**
+     * @param param
+     * @return
+     */
     @Override
     public String doService(String param) {
+
         return "hello, " + param;
     }
 
+    /**
+     * @param param
+     * @return
+     */
     @Override
     public String callService(String param) {
         return param;
